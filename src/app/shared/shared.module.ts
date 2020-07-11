5af830efa42849c758372da670e8cab5761dc19e
Carlos Macêdo <carlosmacedo025@gmail.com>
2020-07-11 18:09:33 -0300

    Small correction

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MenuComponent } from './components/menu/menu.component';
 import * as arrayComponents from './index';
 
 @NgModule({
-  declarations: [MenuComponent, ...arrayComponents.components],
+  declarations: [...arrayComponents.components],
   imports: [CommonModule],
   exports: [...arrayComponents.components],
 })
