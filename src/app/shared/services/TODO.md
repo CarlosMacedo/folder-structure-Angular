f5cfb96bb252b0515a23096f0b8f4919034ba61a
Carlos Macêdo <carlosmacedo025@gmail.com>
2020-07-12 17:09:37 -0300

    Best pratices in font-sizes

---
+++ src/app/shared/services/TODO.md
@@ -0,0 +1,2 @@
+The difference here is that it can be injected into any component.
+While the services on core folder are injected directly into the root.
--- src/app/shared/services/example.service.ts
+++ src/app/shared/services/example.service.ts
@@ -1,8 +1,3 @@
-/*
- * The difference here is that it can be injected into any component.
- * While the services on core folder are injected directly into the root.
- *
- */
 import { Injectable } from '@angular/core';
 
 @Injectable()
