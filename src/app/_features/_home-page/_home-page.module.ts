55fc538fa768f8a1c837108a35513448221e5b47
Carlos Macêdo <carlosmacedo025@gmail.com>
2020-07-15 23:55:28 -0300

    Fixed error @shared

--- src/app/_features/_home-page/_home-page.module.ts
+++ src/app/_features/_home-page/_home-page.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MenuComponent, TitleComponent } from '@shared/components';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [MenuComponent, TitleComponent],
-  imports: [CommonModule],
-  exports: [MenuComponent, TitleComponent],
+  declarations: [],
+  imports: [CommonModule, SharedModule],
+  exports: [SharedModule],
 })
 export class HomePageModule {}
